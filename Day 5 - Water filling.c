8fa016a8ca3fe945a1cb502718c252e926bae293
TechyRohit29 <rohitsaxena8056@gmail.com>
2024-10-02 10:02:16 +0530

    Add files via upload

---
+++ Day 5 - Water filling.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int B1, B2, B3;
+    printf("Enter the status of bottle (0 = Empty, 1 = Filled: ");
+    scanf("%d %d %d", &B1, &B2, &B3);
+    
+    if(B1, B2, B3 == 0) {
+        printf("Water filling time");
+    } else {
+        printf("Not now");
+    }
+}
